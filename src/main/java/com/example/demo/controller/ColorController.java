e5812e7b833b104f435ec99fb4f650cc44f144c5
Rushikesh Patil <39215946+rushikesh-patil@users.noreply.github.com>
2021-09-23 10:52:19 +0530

    color validation controller issue fixes

--- src/main/java/com/example/demo/controller/ColorController.java
+++ src/main/java/com/example/demo/controller/ColorController.java
@@ -16,24 +16,9 @@ public class ColorController {
 
         Color colorObj = new Color();
         colorObj.setColorName(color.getColorName());
-        if(color.equals("RED")){
+        if(color.getColorName().equals("RED")){
             colorObj.setCode("R100");
-        }else if(color.equals("GREEN")){
-            colorObj.setCode("G200");
-        }else{
-            colorObj.setCode("B300");
-        }
-        return colorObj;
-    }
-
-    @GetMapping("/color/{colorName}")
-    public Color getColorCode2(@PathVariable @Valid @ColorValidation String colorName){
-
-        Color colorObj = new Color();
-        colorObj.setColorName(colorName);
-        if(colorName.equals("RED")){
-            colorObj.setCode("R100");
-        }else if(colorName.equals("GREEN")){
+        }else if(color.getColorName().equals("GREEN")){
             colorObj.setCode("G200");
         }else{
             colorObj.setCode("B300");
